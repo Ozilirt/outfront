9ff010fa117514ae29ed8c0672085a7cb7bd7b53
Ozilirt <ozilirt@rambler.ru>
2025-08-11 20:58:52 +0700

    restructured

--- start_editor — копия.bat
+++ start_editor — копия.bat
@@ -1,2 +1,2 @@
 rem to start editor type  soldiers_editor.exe -edit <path to map>:<name of mission>
-soldiers_editor.exe -edit 3-way_to_berlin\3_1-landing:0
+soldiers_editor.exe -edit single\Midzui:0

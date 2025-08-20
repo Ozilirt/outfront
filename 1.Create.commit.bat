c772c5650e14df0711390dd5042e3971db011e74
Ozilirt <ozilirt@rambler.ru>
2025-08-21 00:04:54 +0700

    edit

---
+++ 1.Create.commit.bat
@@ -0,0 +1,7 @@
+f:
+cd F:\Игры\В тылу врага\3
+git init
+git add .
+git status
+pause
+git commit -m "edit"
---
+++ start_editor.bat
@@ -0,0 +1,2 @@
+rem to start editor type  soldiers_editor.exe -edit <path to map>:<name of mission>
+soldiers_editor.exe -edit pacific/k2/k2_5:0

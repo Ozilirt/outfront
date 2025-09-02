c291602eda3331367589e95e7b13ae6fc7993d70
Ozilirt2 <Ozilirt500@gmail.com>
2025-09-02 21:24:53 +0700

    new user commit

--- 1.Create.commit.bat
+++ 1.Create.commit.bat
@@ -1,10 +1,15 @@
+
+:: Для получения пользовательского
+:: FOR /F "tokens=*" %%A IN ('TYPE CON') DO SET INPUT=%%A
+:: ECHO Вы ввели: "%INPUT%"
+pause
 D:
 cd D:\Denis\outfront
 git init
 pause
 git add .
 pause
-git commit -m "new user"
+git commit -m "new user commit"
 pause
 git status
 pause

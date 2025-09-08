77114a46587dd3b6bc0c54887c20f84765d8b8d7
ozilirt <ozilirt@rambler.ru>
2025-09-08 21:26:15 +0700

    new

--- 1.Create.commit.bat
+++ 1.Create.commit.bat
@@ -3,15 +3,14 @@
 :: FOR /F "tokens=*" %%A IN ('TYPE CON') DO SET INPUT=%%A
 :: ECHO Вы ввели: "%INPUT%"
 pause
-f:
-cd F:\\Игры\В тылу врага\3\outfront
+::f:
+::cd F:\\Игры\В тылу врага\3\outfront
 git init
 pause
 git add .
 pause
-git commit -m "new user commit"
+git commit -m "new "
 pause
 git status
-pause
-git commit -m "k3-3-k4-2"
+
 
--- 6.profile.bat
+++ 6.profile.bat
@@ -1,2 +1,2 @@
-git config --global user.name "Ozilirt2"
-git config --global user.email "Ozilirt500@gmail.com"
+git log
+pause
